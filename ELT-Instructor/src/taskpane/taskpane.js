f55a43103936555b7b57922f3daaf7ce729a46d0
c00302990 <c00302990@setu.ie>
2024-02-29 19:29:24 +0900

    modified 02/28/2024

--- ELT-Instructor/src/taskpane/taskpane.js
+++ ELT-Instructor/src/taskpane/taskpane.js
@@ -8,29 +8,36 @@ Office.onReady(function() {
 
 async function extractWords() {
   await Word.run(function(context) {
-    var fileName = Office.context.document.url.split("\\").pop();
-    console.log(fileName.replace(".docx", ""));
+    var fileName = Office.context.document.url.split("\\").pop().replace(".docx", "");
 
     var body = context.document.body;
     body.load("text");
 
     return context.sync().then(function() {
-      var wordsArray = body.text.match(/\b\w+\b/g);
-      var filtered = wordsArray.filter((v, i) => wordsArray.indexOf(v) === i);
-      var j = 0;
-      var result = [];
-      for (var i = 0; i < filtered.length; i++) {
-        if (filtered[i].match(/\b[a-z]+\b/) && filtered[i].length > 2) {
-          result[j] = filtered[i];
-          j++;
+      var wordsArr = body.text.toLocaleLowerCase().split(/\s+/);
+      var filtered = [];
+      var specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+      wordsArr.forEach(function(element){
+      if (element[0] == "'" && element[element.length - 1] == "'"){
+      } else if (specialChar.test(element)){
+        element = element.replace(/[^a-z]/g, '');
+        if(element.length > 1 && element.match(/[a-z]/)){
+          filtered.push(element);
+        }
+      } else{
+        if (element.length > 1 && element.match(/[a-z]/)){
+          filtered.push(element);
         }
       }
-      console.log(result);
+    });
 
+    var uniqueArr = new Set(filtered);
+      console.log(uniqueArr);
+      
       $.ajax({
         url: "http://127.0.0.1:8080/create",
         type: "POST",
-        data: { arrayData: result },
+        data: { arrayData: result, name: fileName },
         success: function(response) {
           console.log("success");
         },
--- web_application/main.js
+++ web_application/main.js
@@ -12,49 +12,158 @@ app.use(bodyParser.urlencoded({extended:false}));
 const helmet = require('helmet');
 app.use(helmet());
 
+/*
+const {GoogleGenerativeAI} = require('@google/generative-ai');
+process.env.API_KEY = 'AIzaSyCbYRFhSVuOCbaE6-fTptVpmc_XvAAYU-s';
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+
+async function run() {
+	const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  
+	const prompt = "Write a story about a magic backpack."
+  
+	const result = await model.generateContent(prompt);
+	const response = await result.response;
+	const text = response.text();
+	console.log(text);
+}
+  
+run(); */
+
+
+
 app.get('/', function (req, res) {
 	var template = `
 	<!DOCTYPE html>
 	<html>
+	<head>
+		<meta http-equiv="Content-Security-Policy" content="script-src 'self' https://appsforoffice.microsoft.com/lib/1.1/hosted/office.js;">
+		<!--<script type="text/javascript" src="https://appsforoffice.microsoft.com/lib/1.1/hosted/office.js"></script>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.js"></script>-->
+	</head>
+	
+	<body>
+		<H1>Quizzes</H1>
+		<main>
+	
+			<p>
+				<button id="create">create</button>
+			</p>
+
+			<p>
+				<select>
+
+				</select>
+			</p>
+			<p>
+				<button id="view">view</button>&nbsp;&nbsp;<button id="delete">delete</button>
+			</p>
+			
+		</main>
+	</body>
+	</html>
+	`; 
+	/*`
+	<!DOCTYPE html>
+	<html>
 		<head>
 			<title>Welcome!</title>
+			
 		</head>
 	
 		<body>
 			<form action="" method="post">
 				<p><input type="text" id="userId" placeholder="ID"></p>
 				<p><input type="password" id="userPwd" placeholder="Password"></p>
-				<p><input type="submit" value="login"></p>
+				<input type="submit" value="login">
 			</form>
+			<button onclick="moveToRegisterPage()">register</button>
+			<p><a href="">forget your id or password?</a></p>
 		</body>
+
+		<script>
+			function moveToRegisterPage(){
+				
+			}
+		</script>
+
 	</html>
-	`;
+	`;*/
+
+
 	res.send(template);
 });
 
+
 app.post('/create', function (req, res) {
 	var received = req.body['arrayData[]'];
+	var excluded = ['is','an'];
 	var result = [];
 	async function compareWords(){
 		for(var word of received){
-			await new Promise(function(resolve,reject){
-				var sql = `SELECT * FROM words WHERE word='${word}';`;
-				maria.query(sql, function(err, rows, fields){
-					if(err){
-						reject(err);
-						return;
-					}
-
-					if(rows.length === 0){
-						result.push(word);
-					}
-
-					resolve();
+			if(!excluded.includes(word)){
+				await new Promise(function(resolve,reject){
+					var sql = `SELECT * 
+								FROM words
+								WHERE word='${word}' OR word='${word.slice(0,word.length-1)}'
+													 OR word='${word.slice(0,word.length-2)}'
+													 OR word='${word.slice(0,word.length-3)}';`;
+				
+					maria.query(sql, function(err, rows, fields){
+						if(err){
+							reject(err);
+							return;
+						}
+	
+						if(rows.length === 0){
+							result.push(word);
+						}
+	
+						resolve();
+					});
 				});
-			});
+			}
+			
 		}
-		
-		console.log(result);
+
+		maria.changeUser({database:'test1'},function(err){
+			if(err){
+				console.error(err);
+			}
+			
+		});
+
+		var tableName = req.body['name'];
+		var createSQL = `CREATE TABLE IF NOT EXISTS ${tableName} (
+			no INT AUTO_INCREMENT,
+			word VARCHAR(30),
+			PRIMARY KEY(no)
+			);`;
+
+
+		maria.query(createSQL, function(err, rows, fields){
+			async function insert(){
+				for(var r of result){
+					await new Promise(function(resolve,reject){
+						var sql = `INSERT INTO ${tableName} (word) VALUES ('${r}')`;
+						
+							maria.query(sql, function(err, rows, fields){
+								if(err){
+									reject(err);
+									return;
+								}
+			
+								resolve();
+							});
+						});
+					}
+					
+				}
+
+			insert();
+		});
 	}
 
 	compareWords();
